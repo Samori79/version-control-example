fb50619f58ecd44e03df63facacc560df32d65c1
Samori Bryan <samori.bryan@mail.utoronto.ca>
2021-03-14 23:13:13 -0400

    test of init command

--- Assets/test to see what init cammade can do.cs
+++ Assets/test to see what init cammade can do.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class challengetest : MonoBehaviour
+public class testtoseewhatinitcammadecando : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
